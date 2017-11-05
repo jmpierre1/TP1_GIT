8f80d42b2b3c033cadf0ce377f6a47f3f2f741e9
Jean.Milou.Pierre <miloujpierre@gmail.com>
2017-11-04 21:10:31 -0400

    Modification de la fonction division

--- module.py
+++ module.py
@@ -5,8 +5,8 @@ def produit(a,b):
 # calcul le quotient de deux nombres
 def division(a,b):
     if(b==0):
-        print("L'entrée fournie est invalide, on ne divise pas par zéro"
-    else:)
+        print("L'entrée fournie est invalide, on ne divise pas par zéro")
+    else:
         return a/b
 
 #calcul la différence de deux nombres
