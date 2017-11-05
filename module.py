ca593d5464cedc0bb063c6e18a3021bd1c422a95
Jean.Milou.Pierre <miloujpierre@gmail.com>
2017-11-04 21:02:49 -0400

    Ajout de deux fonctions (/ et +)

--- module.py
+++ module.py
@@ -1,3 +1,12 @@
 # calcul du produit de deux nombres passés en argument.
 def produit(a,b):
     return a*b
+
+# calcul le quotient de deux nombres
+def division(a,b):
+    return a/b
+
+#calcul la différence de deux nombres
+
+def soustraction(a,b):
+    return b-a
