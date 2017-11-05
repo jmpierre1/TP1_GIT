3a51a350681bcf43222fb3b20602ecfb8a767d10
Jean.Milou.Pierre <miloujpierre@gmail.com>
2017-11-04 21:07:38 -0400

    Gestion triviale d'une exception pour la division par zéro

--- module.py
+++ module.py
@@ -4,7 +4,10 @@ def produit(a,b):
 
 # calcul le quotient de deux nombres
 def division(a,b):
-    return a/b
+    if(b==0):
+        print("L'entrée fournie est invalide, on ne divise pas par zéro"
+    else:)
+        return a/b
 
 #calcul la différence de deux nombres
 
